bb4fb68049a2195ad7e873b5deac9579e5e217d3
JoaoMSO <joaomatiassantosolim@gmail.com>
2025-02-13 23:47:58 +0100

    refactor: Refactor Jeux-Morpion.py for improved readability and maintainability

--- Jeux-Morpion.py
+++ Jeux-Morpion.py
@@ -5,60 +5,60 @@ Created on Thu Feb 13 18:59:39 2025
 @author: JMSO
 
 """
-Lt=[['X',1,2,3],['a',0,0,0],['b',0,0,0],['c',0,0,0]]
+Lt=[
+    ['X',1,2,3],
+    ['a',0,0,0],
+    ['b',0,0,0],
+    ['c',0,0,0]
+]
 mouv_let = 0
 mouv_num = 0
-partie = True
+Etat = True
 nbr_pt = 0
 
-def ecran ():
+def ecran ():           #fonction qui permet d'afficher le tableau
     for i in range(0,4):
         a = Lt[i]
         print(a)
 
-def Mouv_Y():
-    mouv_let=input('choisies une lettre ....')
-    match(mouv_let):
+def Mouv_Y():         #fonction qui permet de choisir un mouvement sur l'axe des y
+    mouv_let=input('choisies une lettre ....         ')
+    match mouv_let:
         case 'a':
-            return int(1)
+            return 1
         case 'b':
-            return int(2)
+            return 2
         case 'c':
-            return int(3)
+            return 3
 
-def Mouv_X():
-    mouv_num=int(input('choisies un chiffre ....'))
+
+def Mouv_X():        #fonction qui permet de choisir un mouvement sur l'axe des x
+    mouv_num=int(input('choisies un chiffre ....     '))
     return mouv_num
 
-def Partie():
+def Partie(mouv_let, mouv_num):         #fonction qui permet de jouer
     if mouv_let != 0 and mouv_num != 0 :
-        Lt[mouv_let][mouv_num] = 11111111111111111
+        Lt[mouv_let][mouv_num] = 1
 
-def main_loop():
-    while partie == True:
+def main_loop():     #fonction princupale
+    while Etat:
         ecran()
-        mouv_let=Mouv_Y()
+        mouv_let = Mouv_Y()
         mouv_num = Mouv_X()
-        Partie()
-        
+        Partie(mouv_let, mouv_num)
+        nbr_pt += 1
+        if nbr_pt == 9:
+            Etat = False
+
+Etat = True
+nbr_pt = 0
+Lt = [
+    ['X', 1, 2, 3],
+    ['a', 0, 0, 0],
+    ['b', 0, 0, 0],
+    ['c', 0, 0, 0]
+]
+
         
 main_loop()
     
-"""
-['X',1,2,3]
-['a',0,0,0]
-['b',0,0,0]
-['c',0,0,0]
-def http_error(status):
-    match status:
-        case 400:
-            return "Bad request"
-        case 404:
-            return "Not found"
-        case 418:
-            return "I'm a teapot"
-
-        # If an exact match is not confirmed, this last case will be used if provided
-        case _:
-            return "Something's wrong with the internet"
-"""
