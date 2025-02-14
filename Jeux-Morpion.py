fccfc7b4bb2c030ea9f5c4cbfc788ba6cc46f6ad
JoaoMSO <joaomatiassantosolim@gmail.com>
2025-02-14 19:45:46 +0100

    Morpion_class.V.1

--- Jeux-Morpion.py
+++ Jeux-Morpion.py
@@ -5,60 +5,60 @@ Created on Thu Feb 13 18:59:39 2025
 @author: JMSO
 
 """
-Lt=[
-    ['X',1,2,3],
-    ['a',0,0,0],
-    ['b',0,0,0],
-    ['c',0,0,0]
-]
-mouv_let = 0
-mouv_num = 0
-Etat = True
-nbr_pt = 0
 
-def ecran ():           #fonction qui permet d'afficher le tableau
-    for i in range(0,4):
-        a = Lt[i]
-        print(a)
-
-def Mouv_Y():         #fonction qui permet de choisir un mouvement sur l'axe des y
-    mouv_let=input('choisies une lettre ....         ')
-    match mouv_let:
-        case 'a':
-            return 1
-        case 'b':
-            return 2
-        case 'c':
-            return 3
-
-
-def Mouv_X():        #fonction qui permet de choisir un mouvement sur l'axe des x
-    mouv_num=int(input('choisies un chiffre ....     '))
-    return mouv_num
-
-def Partie(mouv_let, mouv_num):         #fonction qui permet de jouer
-    if mouv_let != 0 and mouv_num != 0 :
-        Lt[mouv_let][mouv_num] = 1
-
-def main_loop():     #fonction princupale
-    while Etat:
-        ecran()
-        mouv_let = Mouv_Y()
-        mouv_num = Mouv_X()
-        Partie(mouv_let, mouv_num)
-        nbr_pt += 1
-        if nbr_pt == 9:
-            Etat = False
-
-Etat = True
-nbr_pt = 0
-Lt = [
-    ['X', 1, 2, 3],
-    ['a', 0, 0, 0],
-    ['b', 0, 0, 0],
-    ['c', 0, 0, 0]
-]
-
+class MorpionGame:
+    def __init__(self):
+        self.Lt = [
+            ['X', 1, 2, 3],
+            ['a', '/','/', '/'],
+            ['b', '/', '/', '/'],
+            ['c', '/','/', '/']
+        ]
+        self.mouv_let = 0
+        self.mouv_num = 0
+        self.Etat = True
+        self.nbr_pt = 0
+        self.X = 'X'
+        self.O = '0'
         
-main_loop()
-    
+
+    def ecran(self):  # fonction qui permet d'afficher le tableau
+        for i in range(0, 4):
+            a = self.Lt[i]
+            print(a)
+
+    def Mouv_Y(self):  # fonction qui permet de choisir un mouvement sur l'axe des y
+        mouv_let = input('choisies une lettre ....         ')
+        match mouv_let:
+            case 'a':
+                return 1
+            case 'b':
+                return 2
+            case 'c':
+                return 3
+
+    def Mouv_X(self):  # fonction qui permet de choisir un mouvement sur l'axe des x
+        mouv_num = int(input('choisies un chiffre ....     '))
+        return mouv_num
+
+    def Partie(self, mouv_let, mouv_num):  # fonction qui permet de jouer
+        if mouv_let != 0 and mouv_num != 0:
+            if self.nbr_pt % 2 == 0:
+                self.Lt[mouv_let][mouv_num] = self.X
+            else:
+                self.Lt[mouv_let][mouv_num] = self.O
+
+    def main_loop(self):  # fonction principale
+        while self.Etat:
+            self.ecran()
+            mouv_let = self.Mouv_Y()
+            mouv_num = self.Mouv_X()
+            self.Partie(mouv_let, mouv_num)
+            self.nbr_pt += 1
+            if self.nbr_pt == 9:
+                self.Etat = False
+
+
+if __name__ == "__main__":
+    game = MorpionGame()
+    game.main_loop()
